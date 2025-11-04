51c0743eaf72cae5a9d0fc8610f6945828b8941b
Hiru <hirusigan@gmail.com>
2025-11-04 16:13:19 +0100

    test added

--- BettingApi/obj/Debug/net9.0/BettingApi.AssemblyInfo.cs
+++ BettingApi/obj/Debug/net9.0/BettingApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BettingApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd0fb76818629007170b30219b1939e0b6ffb189")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86859f60e0576bed6f6e8891d32ed8920cf20b3e")]
 [assembly: System.Reflection.AssemblyProductAttribute("BettingApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BettingApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
